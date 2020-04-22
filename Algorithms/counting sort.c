73e0da4093d7821cf3512e49f4937e58c73ec159
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2020-04-22 17:43:15 +0600

    counting_sort

---
+++ Algorithms/counting sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void counting_sort(int A[],int mx,int n){
+int count[mx];
+
+for(int i=0;i<mx;i++){
+
+    count[i]=0;
+}
+
+for(int i=0;i<n;i++){
+    int x=A[i];
+    count[x]=count[x]+1;
+
+}
+
+
+for(int i=0;i<mx;i++){
+    for(int j=0;j<count[i];j++){
+        printf("%d",i);
+    }
+}
+
+}
+
+int main(){
+int A[]={3,4,1,6,2,4,9,7,8,4,2,1};
+
+counting_sort(A,10,12);
+
+
+}
